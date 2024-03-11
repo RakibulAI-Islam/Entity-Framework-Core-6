2805d801a81bb1fa333d038fcde717d46fcebb45
Rakibul_AI <rakib.ice07@gmail.com>
2024-03-11 21:06:53 +0600

    1.Create the project & Ensure the Target Framework is net6.0

--- 1_Introduction_CreateAppDbContext/1_EF_DbLibrary/1_EF_DbLibrary/App_DbContext.cs
+++ 1_Introduction_CreateAppDbContext/1_EF_DbLibrary/1_EF_DbLibrary/App_DbContext.cs
@@ -4,6 +4,8 @@ namespace _1_EF_DbLibrary
 {
     public class App_DbContext : DbContext
     {
-
+        public App_DbContext() { } //For Scaffolding.
+        public App_DbContext( DbContextOptions options) : base( options) //For Dependency Injection.
+        { }
     }
 }
