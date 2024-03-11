8cc1f51a926bdf5c3ff79f09786d829264313c61
Rakibul_AI <rakib.ice07@gmail.com>
2024-03-11 22:44:34 +0600

    2.Add 1_EF_DbLibrary project to this project & prepare Program.cs file to Display, Hello, World.

--- 2_Working with an Existing Database/EFCore6_0201/EFCore6_0201/Program.cs
+++ 2_Working with an Existing Database/EFCore6_0201/EFCore6_0201/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace EFCore6_0201
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
