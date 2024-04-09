017721d0d2bb47a936b5b621d67e2dde0d252ba2
Rakbiul_AI <rakib.ice07@gmail.com>
2024-04-09 17:30:29 +0600

    9.Write the IConfigurationRoot type variable _configuration in 'Program.cs' class.

--- 3_CodeFirst_EF/EFCore6_0301/EFCore6_0301/Program.cs
+++ 3_CodeFirst_EF/EFCore6_0301/EFCore6_0301/Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace EFCore6_0301
 {
     class Program
     {
+        private static IConfigurationRoot _configuration;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
