c87440d8618ade2cf96ee35bd90331f0a9a68c2a
Rakbiul_AI <rakib.ice07@gmail.com>
2024-04-09 20:46:11 +0600

    14.Connect 'AdventureWorks_2022Context' via the 'Connection_String'.

--- 3_CodeFirst_EF/EFCore6_0301/EFCore6_0301/Program.cs
+++ 3_CodeFirst_EF/EFCore6_0301/EFCore6_0301/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using _1_EF_DbLibrary;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 
@@ -8,6 +10,7 @@ namespace EFCore6_0301
     class Program
     {
         private static IConfigurationRoot _configuration;
+        private static DbContextOptionsBuilder<AdventureWorks_2022Context> _optionsBuilder;
         static void Main(string[] args)
         {
             BuildConfiguration();
@@ -22,6 +25,13 @@ namespace EFCore6_0301
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("App_Settings.json", optional: true, reloadOnChange: true);
             
             _configuration = builder.Build();
+            //_optionsBuilder.UseSqlServer();
+        }
+
+        static void BuildOptions()
+        {
+            _optionsBuilder = new DbContextOptionsBuilder<AdventureWorks_2022Context>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks_2022"));
         }
 
     }
