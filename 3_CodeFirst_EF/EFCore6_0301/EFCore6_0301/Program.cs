80d60598dbdbe18ade2d2a1466139b0489d203a9
Rakbiul_AI <rakib.ice07@gmail.com>
2024-04-09 11:27:49 +0600

    1.Create a 'Console Project', Ready 'Program.cs class' & 'Test the Output'.

---
+++ 3_CodeFirst_EF/EFCore6_0301/EFCore6_0301/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace EFCore6_0301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+
+    }
+}
+
