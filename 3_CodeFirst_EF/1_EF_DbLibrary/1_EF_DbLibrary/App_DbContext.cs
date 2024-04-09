fe80a521de3ca0c0fc451f634f2916057da2108a
Rakbiul_AI <rakib.ice07@gmail.com>
2024-04-09 12:56:43 +0600

    2.Copy the '1_EF_DbLibrary' file in 'this prject files'.

---
+++ 3_CodeFirst_EF/1_EF_DbLibrary/1_EF_DbLibrary/App_DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _1_EF_DbLibrary
+{
+    public class App_DbContext : DbContext
+    {
+        public App_DbContext() { } //For Scaffolding.
+        public App_DbContext( DbContextOptions options) : base( options) //For Dependency Injection.
+        { }
+        /*public AdventureWorks_2022Context(DbContextOptions<AdventureWorks_2022Context> options)
+            : base(options)
+        {
+        }*/
+    }
+}
