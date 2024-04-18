7169a6474efd4de7f25bae12463dbbe89b7325c0
Rakbiul_AI <rakib.ice07@gmail.com>
2024-04-18 18:30:58 +0600

    22.'Add-Migration' of 'Test_DB_State'.

---
+++ 3_CodeFirst_EF/1_EF_DbLibrary/1_EF_DbLibrary/Migrations/20240418121814_Test_DB_State.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _1_EF_DbLibrary.Migrations
+{
+    public partial class Test_DB_State : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
