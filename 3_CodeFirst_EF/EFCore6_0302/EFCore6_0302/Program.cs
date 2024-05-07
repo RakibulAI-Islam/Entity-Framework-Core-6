f116ae7983db3d0fa517023fdd0ccf1d04c20436
Rakbiul_AI <rakib.ice07@gmail.com>
2024-05-07 19:29:46 +0600

    4.'Prepare' the 'Program.cs' _class.

--- 3_CodeFirst_EF/EFCore6_0302/EFCore6_0302/Program.cs
+++ 3_CodeFirst_EF/EFCore6_0302/EFCore6_0302/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using EFCore6_0302;
+
+public class Program
+{
+    private static IConfigurationRoot _configuration;
+
+    //private static DbContextOptionsBuilder<InventoryDbContext> _optionBuilder;
+
+    static void Main(string[] args)
+    {
+        BuildOptions();
+    }
+
+    static void BuildOptions()
+    {
+        _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+        //_optionBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+        //_optionBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+    }
+}
