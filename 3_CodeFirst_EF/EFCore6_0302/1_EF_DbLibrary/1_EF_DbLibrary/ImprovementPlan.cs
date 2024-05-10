abe981ad06c2d57725db1d4094564fcd83f62f9c
Rakbiul_AI <rakib.ice07@gmail.com>
2024-05-10 14:17:49 +0600

    3.'Copy' the '1_EF_DbLibrary' _folder.

---
+++ 3_CodeFirst_EF/EFCore6_0302/1_EF_DbLibrary/1_EF_DbLibrary/ImprovementPlan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_EF_DbLibrary
+{
+    public class ImprovementPlan
+    {
+        [Key]
+        [ForeignKey("Employee")]
+
+        public int BusinessEntityID { get; set;}
+        public virtual Employee Employee { get; set; }
+
+        [Required]
+        public DateTime PlanStart { get; set; } 
+        public DateTime PlanEnd { get; set; }
+        
+        
+    }
+}
