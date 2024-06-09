3bbc62a6abbac3c601fde3dd4df291fc6dfdd0ec
Rakbiul_AI <rakib.ice07@gmail.com>
2024-06-09 17:01:13 +0600

    20.'Add' the 'ConfigurationBuilderSingleton.cs' _class in 'M_EFCore6_0302' _project.

---
+++ 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/ConfigurationBuilderSingleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_EFCore6_0302
+{
+    internal class ConfigurationBuilderSingleton
+    {
+    }
+}
