5bc233a13a51037c2980444ccdbb33987ba770c3
Rakbiul_AI <rakib.ice07@gmail.com>
2024-06-09 17:45:04 +0600

    22.'Write' the 'Variables' of the 'ConfigurationBuilderSingleton.cs' _class in 'M_EFCore6_0302' _project.

--- 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/ConfigurationBuilderSingleton.cs
+++ 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/ConfigurationBuilderSingleton.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace M_EFCore6_0302
-
+{ 
     //2.public sealed class ConfigurationBuilderSingleton
     //1.public sealed class ConfigurationBuilderSingleton
     public sealed class ConfigurationBuilderSingleton
     {
+        private static ConfigurationBuilderSingleton _instance = null;
+        
+        private static readonly object _instanceLock = new object();
+
+        public static IConfigurationRoot _configuration;
     }
 }
