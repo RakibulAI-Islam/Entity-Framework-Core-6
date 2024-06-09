52edf5ef8fbb658788012d41a894c9ab3d28a5dd
Rakbiul_AI <rakib.ice07@gmail.com>
2024-06-09 18:29:55 +0600

    24.'Write' the 'Access_Modifier' of '_instance' _property of 'ConfigurationBuilderSingleton.cs' _class in 'M_EFCore6_0302' _project.

--- 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/ConfigurationBuilderSingleton.cs
+++ 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/ConfigurationBuilderSingleton.cs
@@ -6,22 +6,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace M_EFCore6_0302
-{ 
+{
     //2.public sealed class ConfigurationBuilderSingleton
     //1.public sealed class ConfigurationBuilderSingleton
     public sealed class ConfigurationBuilderSingleton
     {
         private static ConfigurationBuilderSingleton _instance = null;
-        
+
         private static readonly object _instanceLock = new object();
 
         public static IConfigurationRoot _configuration;
 
+        /*private*/
+
         private ConfigurationBuilderSingleton()
         {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional:true, reloadOnChange:true);
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
             _configuration = builder.Build();
+        }
+
+        /*public*/
+        public static ConfigurationBuilderSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                { _instance = new ConfigurationBuilderSingleton(); }
+
+                return _instance;
+            }
         }
     }
 }
