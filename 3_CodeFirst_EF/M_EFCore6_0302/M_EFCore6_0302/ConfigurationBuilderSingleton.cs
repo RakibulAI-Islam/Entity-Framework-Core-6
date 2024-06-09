c8cfaf8ebc99604017c36564d8b2340f4d21860b
Rakbiul_AI <rakib.ice07@gmail.com>
2024-06-09 17:25:33 +0600

    21.'OK' the 'Access_Modifier' of the 'ConfigurationBuilderSingleton.cs' _class in 'M_EFCore6_0302' _project.

--- 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/ConfigurationBuilderSingleton.cs
+++ 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/ConfigurationBuilderSingleton.cs
@@ -5,8 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace M_EFCore6_0302
-{
-    internal class ConfigurationBuilderSingleton
+
+    //2.public sealed class ConfigurationBuilderSingleton
+    //1.public sealed class ConfigurationBuilderSingleton
+    public sealed class ConfigurationBuilderSingleton
     {
     }
 }
