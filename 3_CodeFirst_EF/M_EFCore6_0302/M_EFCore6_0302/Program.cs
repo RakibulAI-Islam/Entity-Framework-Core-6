c0bdd16ed46978502b325b16a16f5add0c61aacc
Rakbiul_AI <rakib.ice07@gmail.com>
2024-06-09 16:16:36 +0600

    17.'Start_Writing' the 'Program.cs' _class in 'M_EFCore6_0302' _project.

--- 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/Program.cs
+++ 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/Program.cs
@@ -1,3 +1,8 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+public class Program
+{
+
+}
+
