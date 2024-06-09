1e637a892cbf08a5b04c81250f9b9cbe089085bf
Rakbiul_AI <rakib.ice07@gmail.com>
2024-06-09 16:25:06 +0600

    18.'Write' the 'Main()' _method in 'Program.cs' _class in 'M_EFCore6_0302' _project.

--- 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/Program.cs
+++ 3_CodeFirst_EF/M_EFCore6_0302/M_EFCore6_0302/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-public class Program
-{
+using System.Net.NetworkInformation;
 
+public class Program
+{ 
+    static void Main(string[] args)
+    {
+        //BuildOptions();
+    }
 }
 
